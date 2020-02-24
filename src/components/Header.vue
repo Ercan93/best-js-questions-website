2ce3565217bfb387d67ea1dde342277046efe6da
Ercan Uzunsakal <ercan112358@gmail.com>
2020-02-24 20:59:06 +0300

    Created Header.vue

---
+++ src/components/Header.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="container">
+    <h1 class="logo-name">JS Questions</h1>
+    <nav class="menu">
+      <a class="menu-item">Profile</a>
+      <a class="menu-item">Github</a>
+      <a class="menu-item">About</a>
+    </nav>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  background-color: rgb(255, 230, 0);
+  height: 10vh;
+  display: flex;
+  align-items: center;
+  border-radius: 0 0 20px 20px;
+}
+.menu {
+  margin-left: auto;
+  display: flex;
+}
+.menu-item {
+  margin: 10px 40px 10px 0;
+  font-weight: bold;
+}
+.logo-name {
+  margin-left: 30px;
+}
+</style>
