dec13be4e6184664831029ca47a0e1cf315540ec
Ercan Uzunsakal <ercan112358@gmail.com>
2020-02-25 15:30:05 +0300

    Axios & vue-highlightjs added.

--- src/main.js
+++ src/main.js
@@ -1,13 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
 import store from "./store/index"
-
+import router from "./router/index"
 import Vuesax from "vuesax"
+import VueHighlightJS from 'vue-highlightjs'
 import 'vuesax/dist/vuesax.css'
 Vue.use(Vuesax);
+Vue.use(VueHighlightJS)
 
 new Vue({
         el: '#app',
         store,
+        router,
         render: h => h(App)
 })
