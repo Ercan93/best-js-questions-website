d7efa9464b5f4f05eead2e2a982745b2515df46a
Ercan Uzunsakal <ercan112358@gmail.com>
2020-02-26 23:24:10 +0300

    Update QuestionPage.vue

--- src/views/QuestionPage.vue
+++ src/views/QuestionPage.vue
@@ -1,26 +1,49 @@
 <template>
   <div class="container">
     <div class="questions">
-      <pre v-highlightjs="sourcecode"><code class="javascript"></code></pre>
+      <div class="wrapper">
+        <h2>{{header}}</h2>
+        <pre class="language-js"><code>{{sourcecode}}</code></pre>
+        <div class="selections">
+          <vs-radio
+            class="radio-item"
+            warn
+            v-model="picked"
+            v-for="(data,index) in selections"
+            :val="++index"
+          >
+            <pre class="language-js"><code>{{data}}</code></pre>
+          </vs-radio>
+        </div>
+      </div>
     </div>
-    <div class="solutions"></div>
+    <div class="solutions">
+      <vue-markdown :source="exp"></vue-markdown>
+    </div>
   </div>
 </template>
 <script>
 import axios from "axios";
-
+import VueMarkdown from "vue-markdown";
 export default {
+  components: { VueMarkdown },
   data() {
     return {
-      sourcecode: null
+      sourcecode: null,
+      picked: 0,
+      selections: null,
+      header: null,
+      exp: null
     };
   },
-  created() {
+  mounted() {
     axios
       .get("https://js-best-tactical-questions.firebaseio.com/veri.json")
       .then(response => {
-        console.log(response.data.Q1);
-        this.sourcecode = response.data.A1;
+        this.selections = response.data.A1.split("\n");
+        this.sourcecode = response.data.Q1;
+        this.header = response.data.Header;
+        this.exp = response.data.exp;
       });
   }
 };
@@ -37,6 +60,18 @@ export default {
 .questions {
   width: 50vw;
   height: 90vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-evenly;
+}
+
+.selections {
+  display: flex;
+  flex-direction: column;
+}
+.radio-item {
+  margin-right: auto;
 }
 @media screen and (max-width: 840px) {
   .container {
