7af002dc178e392fc34b5bebcf02014afa91d270
Ercan Uzunsakal <ercan112358@gmail.com>
2020-08-06 19:14:46 +0300

    Update QuestionPage.vue

--- src/views/QuestionPage.vue
+++ src/views/QuestionPage.vue
@@ -2,28 +2,28 @@
   <div class="container">
     <div class="questions">
       <div class="wrapper">
-        <h2>{{header}}</h2>
-        <pre class="language-js"><code>{{sourcecode}}</code></pre>
-        <div class="selections">
-          <vs-radio
-            class="radio-item"
-            dark
-            v-model="picked"
-            v-for="(data,index) in selections"
-            :val="++index"
-          >
-            <pre class="language-js"><code>{{data}}</code></pre>
-          </vs-radio>
-        </div>
+        <h2>{{question.header}}</h2>
+        <pre class="language-js"> <code v-html="question.sourceCode"></code> </pre>
+      </div>
+      <div class="selections">
+        <vs-radio
+          class="radio-item"
+          dark
+          v-model="picked"
+          v-for="(data,index) in question.selections"
+          :val="++index"
+        >
+          <pre class="language-js"><code v-html="data"></code></pre>
+        </vs-radio>
       </div>
     </div>
     <div class="solutions">
       <div class="answer" v-if="showAnswer">
-        <h2>Answer</h2>
-        <vue-markdown class="answerText" :source="exp"></vue-markdown>
-        <vs-button class="next-question" dark border @click="nextQuestion">Next question</vs-button>
+        <h2>Cevap</h2>
+        <vue-markdown class="answerText" :source="question.description"></vue-markdown>
+        <vs-button class="next-question" dark border @click="nextQuestion">Sonraki Soru</vs-button>
       </div>
-      <vs-button class="showAnswer" dark border @click="showAnswerFunc" v-else>Show Answer</vs-button>
+      <vs-button class="showAnswer" dark border @click="showAnswerFunc" v-else>Cevabı göster</vs-button>
     </div>
   </div>
 </template>
@@ -34,54 +34,55 @@ export default {
   components: { VueMarkdown },
   data() {
     return {
-      sourcecode: null,
       picked: 0,
-      selections: null,
-      header: null,
-      exp: null,
-      trueAnswer: null,
-      showAnswer: false
+      number: 0,
+      showAnswer: false,
+      question: null,
+      questions: null,
     };
   },
   methods: {
     showAnswerFunc() {
       this.showAnswer = true;
-    }
+    },
+    nextQuestion() {
+      this.showAnswer = false;
+      this.number = this.number + 1;
+    },
+  },
+  watch: {
+    number(val) {
+      this.question = this.questions[val];
+    },
+  },
+  computed: {
+    question() {
+      return this.question;
+    },
   },
   mounted() {
-    axios
-      .get("https://js-best-tactical-questions.firebaseio.com/veri.json")
-      .then(response => {
-        this.selections = response.data.A1.split("\n");
-        this.sourcecode = response.data.Q1;
-        this.header = response.data.Header;
-        this.exp = response.data.exp;
-        this.trueAnswer = response.data.trueAnswer;
-      });
-  }
+    this.questions = this.$store.getters.getQuestions;
+    this.question = this.questions[0];
+    this.sourceCode = this.question.sourceCode;
+  },
 };
 </script>
 <style scoped>
 .container {
   height: 90vh;
   display: flex;
+  flex-direction: column;
   line-height: 30px;
   font-weight: bold;
 }
 .solutions {
-  width: 60vw;
-  height: 90vh;
   display: flex;
   padding-right: 20px;
-  flex-direction: column;
   align-items: center;
   justify-content: center;
 }
 .questions {
-  width: 40vw;
-  height: 90vh;
   display: flex;
-  flex-direction: column;
   align-items: center;
   justify-content: space-evenly;
 }
