0038d0a794f4c00f3bc4104e0e5d826e13bdc20f
Ercan Uzunsakal <ercan112358@gmail.com>
2020-08-07 21:28:49 +0300

    typos

--- src/main.js
+++ src/main.js
@@ -1,18 +1,18 @@
-import Vue from 'vue'
-import App from './App.vue'
-import store from "./store/index"
-import router from "./router/index"
-import Vuesax from "vuesax"
-import VuePrism from 'vue-prism'
-import 'prismjs/themes/prism.css'
-import 'vuesax/dist/vuesax.css'
+import Vue from "vue";
+import App from "./App.vue";
+import store from "./store";
+import router from "./router";
+import Vuesax from "vuesax";
+import VuePrism from "vue-prism";
+import "prismjs/themes/prism.css";
+import "vuesax/dist/vuesax.css";
 
 Vue.use(Vuesax);
-Vue.use(VuePrism)
+Vue.use(VuePrism);
 
 new Vue({
-        el: '#app',
-        store,
-        router,
-        render: h => h(App)
-})
+  el: "#app",
+  store,
+  router,
+  render: h => h(App)
+});
--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,23 @@
-import Vue from "vue"
-import VueRouter from "vue-router"
+import Vue from "vue";
+import VueRouter from "vue-router";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
-import home from "../views/Home"
-import questionPage from "../views/QuestionPage"
+import Home from "../views/Home";
+import QuestionPage from "../views/QuestionPage";
 
 export default new VueRouter({
-        model: 'history',
-        routes: [
-                {
-                        path: '/',
-                        component: home,
-                        name: "home"
-                },
-                {
-                        path: '/questions',
-                        component: questionPage,
-                        name: "questionPage"
-                }
-        ]
+  mode: "history",
+  routes: [
+    {
+      path: "/",
+      component: Home,
+      name: "home"
+    },
+    {
+      path: "/questions",
+      component: QuestionPage,
+      name: "questionPage"
+    }
+  ]
 });
