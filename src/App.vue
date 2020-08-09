51636d8da326e0bc574adb7160546ccddcc7f647
Ercan Uzunsakal <ercan112358@gmail.com>
2020-08-09 17:32:20 +0300

    Css color added.

--- src/App.vue
+++ src/App.vue
@@ -20,6 +20,15 @@ export default {
 
 <style>
 @import url("https://fonts.googleapis.com/css2?family=Fira+Code:wght@300;400;500&display=swap");
+:root {
+  --yellow: rgb(255, 230, 0);
+  --primary: #5b3cc4;
+  --success: rgb(23, 201, 100);
+  --danger: rgb(242, 19, 93);
+  --warning: rgb(255, 130, 0);
+  --dark: rgb(36, 33, 69);
+  --white: rgb(255, 254, 247);
+}
 * {
   margin: 0;
   padding: 0;
@@ -29,6 +38,11 @@ export default {
 }
 
 #app {
-  background-color: rgb(255, 230, 0);
+  background-color: var(--dark);
+  color: var(--success);
+}
+.btn:hover {
+  color: var(--yellow);
+  box-shadow: 3px 3px white;
 }
 </style>
